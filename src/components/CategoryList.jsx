f3ce6618f06449981d579493d2565a34985469f5
Avinash Powar <powaravinash06@gmail.com>
2025-07-09 09:46:37 +0530

    make the complete  accordian

--- src/components/CategoryList.jsx
+++ src/components/CategoryList.jsx
@@ -3,35 +3,37 @@ import ItemList from "./ItemList";
 import { ORDER_URL } from "../utils/constants";
 
 const CategoryList = ({ data, className }) => {
-  // console.log(data);
-  const { subTitle, title } = data.card.info;
-  const { name, defaultPrice, description, imageId } = data?.card?.info;
+  // console.log(data.card.info.offerTags[0]);
+  // const { subTitle, title } = data.card.info.offerTags[0];
+  const { name, price, defaultPrice, description, imageId } = data?.card?.info;
   return (
-    <div className="border-b-1 border-b-gray-300    w-full">
-      <div className="flex  justify-between my-4">
-        <div className="w-9/12 ">
-          <h1 className="text-xl font-medium">{name}</h1>
-          <h1 className="font-bold text-lg">
-            ₹{defaultPrice / 100}{" "}
-            <span className="text-gray-500 text-sm">
-              🏷️ {title + " " + subTitle}
-            </span>
-          </h1>
-          <p className="text-base text-gray-500 mt-3">{description}</p>
-        </div>
-        <div className="w-3/12 h- ml-50 ">
+    <div className="border-b-1 flex justify-between border-b-gray-300 w-full mb-3 mt-3">
+      {/* <div className="flex w-8/12   my-4"> */}
+      <div className="w-9/12 mb-5 px-10">
+        <h1 className="text-xl font-medium">{name}</h1>
+        <h1 className="font-bold text-lg">
+          ₹{(defaultPrice ?? price) / 100}{" "}
+          <span className="text-gray-500 text-sm">
+            {/* 🏷️ {title + " " + subTitle} */}
+          </span>
+        </h1>
+        <p className="text-base text-gray-500 mt-3 line-clamp-2">
+          {description}
+        </p>
+      </div>
+      <div className="w-3/12 mb-5">
+        <div className="relative">
           <img
-            className="rounded-lg "
+            className="rounded-lg w-1xl h-44 "
             src={ORDER_URL + imageId}
             alt="order-img"
           />
-          <div className="mt-1">
-            <button className="px-10 py-2 bg-white text-green-500 font-semibold border border-gray-300 rounded-2xl cursor-pointer hover:bg-gray-100 ml-6 ">
-              Add
-            </button>
-          </div>
+          <button className="absolute bottom-2 ml-8  px-10 py-2 bg-white text-green-500 font-semibold border border-gray-300 rounded-2xl cursor-pointer hover:bg-gray-100 ">
+            Add
+          </button>
         </div>
       </div>
+      {/* </div> */}
     </div>
   );
 };
--- src/components/DisplayCategory.jsx
+++ src/components/DisplayCategory.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import CategoryList from "./CategoryList";
 
 const DisplayCategory = (props) => {
@@ -7,24 +7,40 @@ const DisplayCategory = (props) => {
   const { title, itemCards } = data;
   const itemList = data.itemCards;
   // console.log(itemList);
+
+  const [shwoItem, setShowItem] = useState(false);
+
+  const handleClick = () => {
+    console.log("clicked");
+    setShowItem(!shwoItem);
+  };
+
   return (
     <div className="mt-8">
-      <div>
+      <div className="border-b-1 border-b-gray-300">
         {/* {header} */}
-        <h1 className="mb-7 text-xl font-bold">
-          {title} ({itemCards.length})
-        </h1>
-        {itemList.map((c, index) => (
-          <CategoryList
-            className={`pb-4 ${
-              index !== itemList.length - 1
-                ? "border-b border-gray-300 mt-5 mb-5"
-                : ""
-            }`}
-            key={index}
-            data={c}
-          />
-        ))}
+        <div className="flex justify-between">
+          <h1
+            className="mb-7 text-xl font-bold cursor-pointer"
+            onClick={handleClick}
+          >
+            {title} ({itemCards.length})
+          </h1>
+          <span className="mr-5">⬇️</span>
+        </div>
+
+        {shwoItem &&
+          itemList.map((c, index) => (
+            <CategoryList
+              className={`pb-4 ${
+                index !== itemList.length - 1
+                  ? "border-b border-gray-300 mt-5 mb-5"
+                  : ""
+              }`}
+              key={index}
+              data={c}
+            />
+          ))}
       </div>
     </div>
   );
